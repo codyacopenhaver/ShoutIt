c2b4dda4d0758a5f050ffeb98047211154e07d81
Cody Copenhaver <cody.a.copenhaver@gmail.com>
2017-06-11 22:13:39 -0400

    Projects in PHP and MySQL by Eduonix Learning Solutions

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+//connect to MySQL
+//mysqli_connect(host,username,password,database name)
+$con = mysqli_connect("","","","");
+
+//test connection
+if(mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
